fcef9974539813457af5332982de7d0d2513d2d8
Fidel Morales <fidel.morales.briones@gmail.com>
2024-10-21 16:35:17 -0600

    evidencia 1

--- string_algorithms/manacher_z/manacher_z_string.cpp
+++ string_algorithms/manacher_z/manacher_z_string.cpp
@@ -6,39 +6,18 @@
 #include <sstream>
 using namespace std;
 
-vector<int> computeZArray(string S) {
-    int n = S.size();
+    vector<int> computeZArray(string S) {
+        int n = S.size();
 
-    vector<int> Z(n);
-    
-    int l, r, k = 0;
+        vector<int> Z(n);
+        
+        int l, r, k = 0;
 
-    for (int i = 1; i < n; i++) {
-        // Caso 1: en caso de que i este fuera de la "Z box"
-        if (i > r) {
-            l = i;
-            r = i;
-
-            while (r < n && S[r - l] == S[r]) {
-                r = r + 1;
-            }
-
-            Z[i] = r - l;
-            r = r - 1;
-        }
-        // si i está dentro de la Z box
-        else {
-            k = i - l;
-
-            // Caso 2: el tamaño del substring actual dentro de la z box es MAYOR al valor z en la posición k, esto
-            // significa que NO ES posible que haya un prefijo más grande después de la z box
-            if (Z[k] < r - i + 1) {
-                Z[i] = Z[k];
-            }
-            // Caso 3: el tamaño del substring actual dentro de la z box es MENOR O IGUAL al valor z en la posición k, esto
-            // significa que ES posible que haya un prefijo más grande después de la z box
-            else if (Z[k] >= r - i + 1) {
+        for (int i = 1; i < n; i++) {
+            // Caso 1: en caso de que i este fuera de la "Z box"
+            if (i > r) {
                 l = i;
+                r = i;
 
                 while (r < n && S[r - l] == S[r]) {
                     r = r + 1;
@@ -47,11 +26,32 @@ vector<int> computeZArray(string S) {
                 Z[i] = r - l;
                 r = r - 1;
             }
-        }
-    }
+            // si i está dentro de la Z box
+            else {
+                k = i - l;
 
-    return Z;
-}
+                // Caso 2: el tamaño del substring actual dentro de la z box es MAYOR al valor z en la posición k, esto
+                // significa que NO ES posible que haya un prefijo más grande después de la z box
+                if (Z[k] < r - i + 1) {
+                    Z[i] = Z[k];
+                }
+                // Caso 3: el tamaño del substring actual dentro de la z box es MENOR O IGUAL al valor z en la posición k, esto
+                // significa que ES posible que haya un prefijo más grande después de la z box
+                else if (Z[k] >= r - i + 1) {
+                    l = i;
+
+                    while (r < n && S[r - l] == S[r]) {
+                        r = r + 1;
+                    }
+
+                    Z[i] = r - l;
+                    r = r - 1;
+                }
+            }
+        }
+
+        return Z;
+    }
 
 unordered_set<int> Z(string T, string P) {
     // crear vector que almacena los índices en donde se encuentra el patrón dado
